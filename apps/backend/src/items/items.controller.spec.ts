7c089e73ebc3a508c72f41cc2c7c6519f694405c
Chirasin Chuenthed <chirasin.chu+skyaidev@skyai.co.th>
2025-09-06 23:15:57 +0700

    gogo backend

---
+++ apps/backend/src/items/items.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ItemsController } from './items.controller';
+
+describe('ItemsController', () => {
+  //test comment
+  let controller: ItemsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ItemsController],
+    }).compile();
+
+    controller = module.get<ItemsController>(ItemsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
+
